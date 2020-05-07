e249a246afc30c841f0725db87a355dc7bda57d9
Stefan Fluit <fluit@datlinq.com>
2020-05-08 00:51:01 +0200

    Added explanation on installation and basic usage

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-1
+
+`certificates.datlinq.com`
+==========
+Installation and user guide.
+
+Installation
+===========
+
+In a terminal of Linux distro of your choice:  
+
+```
+$ sh -c "$(curl -fsSL https://raw.githubusercontent.com/stefanfluit/certbot-superior/master/configure.sh)"
+```
+
+This will configure all the files and programs we need. You can now check the service:
+
+```
+$ systemctl status certbot-superior.service
+```
+
+Make sure to edit the files in /var/lib/scripts, to make sure you set the right API key and other variables.
+
+```
+$ vim /var/lib/scripts/certbot-auto-renew.sh
+```
+
+And:  
+
+```
+$ vim /var/lib/scripts/cleanup.sh
+```
+
+And:  
+
+```
+$ vim /var/lib/scripts/prehook.sh
+```
+
+Systemd will now check every night 01:00 if there is a need to renew the cert.
+
+It defaults to seven days, so if the certificate from a certain host expires in <7 days, it initiates a renewal.
+
+This will get you a domain.pem file in /etc/certbot/domain. You will have to take care of the rest. 
+
+This is written because there is no DNS plugin available for Superior and i simple needed it. 
+
+Work in progress
+===========
+
+This is a work in progress and will be updated soon.
+
--- configure.sh
+++ configure.sh
@@ -13,18 +13,20 @@ if [ "${1}" == '--update' ]; then
     cp -a /var/lib/repos/certbot-superior/scripts/. /var/lib/scripts
     systemctl daemon-reload
 else
-    cp -a /var/lib/repos/certbot-superior/scripts/. /var/lib/scripts/
-    cp -a /var/lib/repos/certbot-superior/systemd/. /etc/systemd/system/
-    systemctl daemon-reload && systemctl start certbot-superior.timer
-    systemctl enable certbot-superior.timer
     declare YUM_CMD
     YUM_CMD=$(which yum)
     declare APT_GET_CMD
     APT_GET_CMD=$(which apt-get)
     if [[ -n "${YUM_CMD}" ]]; then
-        yum install -y openssl
+        yum install -y openssl curl git
     elif [[ -n $APT_GET_CMD ]]; then
-        apt-get install -y openssl
+        apt-get install -y openssl curl git
     fi
+    mkdir -pv /var/lib/scripts
+    mkdir -pv /var/lib/repos && git clone --recursive https://github.com/stefanfluit/certbot-superior.git
+    cp -a /var/lib/repos/certbot-superior/scripts/. /var/lib/scripts/
+    cp -a /var/lib/repos/certbot-superior/systemd/. /etc/systemd/system/
+    systemctl daemon-reload && systemctl start certbot-superior.timer
+    systemctl enable certbot-superior.timer
     printf "Setup done, make sure to edit the domain_names file in the repo/files.\n"
 fi
